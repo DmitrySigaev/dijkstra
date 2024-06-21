85dac54dafcb2e1a992d080e2439d1389137fab0
Dmitry Sigaev <dsigaev@yandex.ru>
2024-06-21 19:58:52 +0300

    added priority queue

--- dijkstra_test.go
+++ dijkstra_test.go
@@ -1,6 +1,7 @@
 package dijkstra
 
 import (
+	"log"
 	"os"
 	"reflect"
 	"strconv"
@@ -8,27 +9,136 @@ import (
 )
 
 func TestNoPath(t *testing.T) {
-	testSolution(t, BestPath{}, ErrNoPath, "testdata/I.txt", 0, 4, true)
+	testSolution(t, BestPath{}, ErrNoPath, "testdata/I.txt", 0, 4, true, -1)
 }
 
 func TestLoop(t *testing.T) {
-	testSolution(t, BestPath{}, newErrLoop(1, 2), "testdata/J.txt", 0, 4, true)
+	testSolution(t, BestPath{}, newErrLoop(2, 1), "testdata/J.txt", 0, 4, true, -1)
 }
 
 func TestCorrect(t *testing.T) {
-	testSolution(t, getBSol(), nil, "testdata/B.txt", 0, 5, true)
-	testSolution(t, getKSolLong(), nil, "testdata/K.txt", 0, 4, false)
-	testSolution(t, getKSolShort(), nil, "testdata/K.txt", 0, 4, true)
+	testSolution(t, getBSol(), nil, "testdata/B.txt", 0, 5, true, -1)
+	testSolution(t, getKSolLong(), nil, "testdata/K.txt", 0, 4, false, -1)
+	testSolution(t, getKSolShort(), nil, "testdata/K.txt", 0, 4, true, -1)
 }
-func BenchmarkDmitrySIgaevNodes4(b *testing.B)    { benchmarkAlt(b, 4, 0) }
-func BenchmarkDmitrySIgaevNodes10(b *testing.B)   { benchmarkAlt(b, 10, 0) }
-func BenchmarkDmitrySIgaevNodes100(b *testing.B)  { benchmarkAlt(b, 100, 0) }
-func BenchmarkDmitrySIgaevNodes1000(b *testing.B) { benchmarkAlt(b, 1000, 0) }
 
+func TestCorrectAllLists(t *testing.T) {
+	for i := 0; i <= 3; i++ {
+		testSolution(t, getBSol(), nil, "testdata/B.txt", 0, 5, true, i)
+		testSolution(t, getKSolLong(), nil, "testdata/K.txt", 0, 4, false, i)
+		testSolution(t, getKSolShort(), nil, "testdata/K.txt", 0, 4, true, i)
+	}
+}
+
+func TestCorrectAutoLargeList(t *testing.T) {
+	g := NewGraph()
+	for i := 0; i < 2000; i++ {
+		v := g.AddNewVertex()
+		v.AddArc(i+1, 1)
+	}
+	g.AddNewVertex()
+	_, err := g.Shortest(0, 2000)
+	testErrors(t, nil, err, "manual test")
+	_, err = g.Longest(0, 2000)
+	testErrors(t, nil, err, "manual test")
+}
+
+var benchNames = []string{"github.com/DmitrySigaev"}
+var listNames = []string{"PQShort", "PQLong", "LLShort", "LLLong"}
+
+func BenchmarkSetup(b *testing.B) {
+	nodeIterations := 6
+	nodes := 1
+	for j := 0; j < nodeIterations; j++ {
+		nodes *= 4
+		b.Run("setup/"+strconv.Itoa(nodes)+"Nodes", func(b *testing.B) {
+			filename := "testdata/bench/" + strconv.Itoa(nodes) + ".txt"
+			if _, err := os.Stat(filename); err != nil {
+				g := Generate(nodes)
+				err := g.ExportToFile(filename)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+			g, _ := Import(filename)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				g.setup(true, 0, -1)
+			}
+		})
+	}
+}
+
+func BenchmarkLists(b *testing.B) {
+	nodeIterations := 6
+	shortest := false
+	shortText := []string{"Short", "Long"}
+	for z := 0; z < 2; z++ {
+		shortest = !shortest
+		for i, n := range listNames {
+			nodes := 1
+			for j := 0; j < nodeIterations; j++ {
+				nodes *= 4
+				b.Run(shortText[z]+"/"+n+"/"+strconv.Itoa(nodes)+"Nodes", func(b *testing.B) {
+					benchmarkList(b, nodes, i, shortest)
+				})
+			}
+		}
+	}
+}
+
+func benchmarkList(b *testing.B, nodes, list int, shortest bool) {
+
+	filename := "testdata/bench/" + strconv.Itoa(nodes) + ".txt"
+	if _, err := os.Stat(filename); err != nil {
+		g := Generate(nodes)
+		err := g.ExportToFile(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	graph, _ := Import(filename)
+	src, dest := 0, len(graph.Verticies)-1
+	//====RESET TIMER BEFORE LOOP====
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		graph.setup(shortest, src, list)
+		graph.postSetupEvaluate(src, dest, shortest)
+	}
+}
+
+func BenchmarkAll(b *testing.B) {
+	nodeIterations := 6
+	for i, n := range benchNames {
+		nodes := 1
+		for j := 0; j < nodeIterations; j++ {
+			nodes *= 4
+			b.Run(n+"/"+strconv.Itoa(nodes)+"Nodes", func(b *testing.B) {
+				benchmarkAlt(b, nodes, i)
+			})
+
+		}
+	}
+	//Cleanup
+	nodes := 1
+	for j := 0; j < nodeIterations; j++ {
+		nodes *= 4
+		os.Remove("testdata/bench/" + strconv.Itoa(nodes) + ".txt")
+	}
+}
+
+/*
+//Mattomatics does not work.
+func BenchmarkMattomaticNodes4(b *testing.B)    { benchmarkAlt(b, 4, 3) }
+*/
 func benchmarkAlt(b *testing.B, nodes, i int) {
 	filename := "testdata/bench/" + strconv.Itoa(nodes) + ".txt"
 	if _, err := os.Stat(filename); err != nil {
-		Generate(nodes, filename)
+		g := Generate(nodes)
+		err := g.ExportToFile(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 	switch i {
 	case 0:
@@ -48,7 +158,7 @@ func benchmarkRC(b *testing.B, filename string) {
 	}
 }
 
-func testSolution(t *testing.T, best BestPath, wanterr error, filename string, from, to int, shortest bool) {
+func testSolution(t *testing.T, best BestPath, wanterr error, filename string, from, to int, shortest bool, list int) {
 	var err error
 	var graph Graph
 	graph, err = Import(filename)
@@ -56,12 +166,19 @@ func testSolution(t *testing.T, best BestPath, wanterr error, filename string, f
 		t.Fatal(err, filename)
 	}
 	var got BestPath
-	if shortest {
+	if list >= 0 {
+		graph.setup(shortest, from, list)
+		got, err = graph.postSetupEvaluate(from, to, shortest)
+	} else if shortest {
 		got, err = graph.Shortest(from, to)
 	} else {
 		got, err = graph.Longest(from, to)
 	}
 	testErrors(t, wanterr, err, filename)
+	testResults(t, got, best, shortest, filename)
+}
+
+func testResults(t *testing.T, got, best BestPath, shortest bool, filename string) {
 	distmethod := "Shortest"
 	if !shortest {
 		distmethod = "Longest"
